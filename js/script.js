9f2e8d93b40b8d8188a5f3998605cf1640553c6c
XuanLyu <lx969181852@gmail.com>
2020-10-07 01:29:22 +1000

    add remove options function

--- js/script.js
+++ js/script.js
@@ -33,13 +33,37 @@ function updateResult(query) {
 }
 
 var indexOfLang = 0;
+// var opDic = {};
 function generateBox(boxContent){
     $('.result').hide();
     let resultList = document.querySelector(".result");
     // var div=document.createElement('div');
     var div = $('#optionsBox');
-    div.append('<p class="options" id="option-' +indexOfLang+ '" onclick="deleteOption(this)"></p>');
-    var langOption = $('#option-' + indexOfLang);
+    var word = boxContent+"";
+    div.append('<p class="options" id="option-' +indexOfLang+'-'+ word+'" onclick = "deleteOption(this)"></p>');
+    Array.prototype.indexOf = function(val) {
+        for (var i = 0; i < this.length; i++) {
+         if (this[i] == val) {
+             return i;
+         }
+     }
+     return -1;
+    };
+    Array.prototype.remove = function(val) {
+        var index = this.indexOf(val);
+        if (index > -1) {
+            this.splice(index, 1);
+        }
+    }
+    // opDic[indexOfLang] = word;
+    var langOption = $('#option-' + indexOfLang + '-'+word);
+    document.getElementById('option-' + indexOfLang + '-' + word).addEventListener("click", function(){
+        console.log(options);
+        arr.push(word);
+        options.remove(word);
+    });
+
+    arr.remove(boxContent);
     langOption.text(boxContent + " x");
     langOption.css('width', 88);
     langOption.css('height', 20);
@@ -60,26 +84,33 @@ function generateBox(boxContent){
             langOption.css('height', 20);
       });
     indexOfLang++;
-    options.push(boxContent);
+    options.push(word);
     console.log(options);
     $("#tags").val("");
 }
 
-function deleteOption(option){
+function deleteOption(option){   
     Array.prototype.indexOf = function(val) {
         for (var i = 0; i < this.length; i++) {
-        if (this[i] == val) return i;
-        }
-        return -1;
-        };
+         if (this[i] == val) {
+             return i;
+         }
+     }
+     return -1;
+    };
     Array.prototype.remove = function(val) {
-        var index = this.indexOf(val);           
-        if (index > -1) {   
-        this.splice(index, 1);          
-        }    
-        };
-    
+        var index = this.indexOf(val);
+        if (index > -1) {
+            this.splice(index, 1);
+        }
+    }
+    // options.remove();
+
     option.remove();
+}
+
+function deleteLangInList(word){
+
 }
 
 function getOptions(){
